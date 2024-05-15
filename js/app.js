f6ce4ab699cc57f97d13019406459e123c4a1024
Raisybear <elias.spycher@gmail.com>
2024-05-15 21:47:39 +0200

    Background Color Fade

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Timeout, um den weißen Bildschirm für 3 Sekunden zu halten
   setTimeout(() => {
     document.body.classList.remove("preload");
   }, 3000);
+
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Verhindert das Standardverhalten des Links
+      const sectionId = this.getAttribute('href').substring(1);
+      showSection(sectionId);
+    });
+  });
 });
 
 function showSection(sectionId) {
-  const sections = document.querySelectorAll('section');
+  const sections = document.querySelectorAll("section");
   sections.forEach(section => {
-    section.classList.remove('active');
+    section.classList.remove("active");
   });
   const activeSection = document.getElementById(sectionId);
-  activeSection.classList.add('active');
+  if (activeSection) {
+    activeSection.classList.add("active");
+  }
+
+  // Verhindert das Scrollen zum Abschnitt
+  window.scrollTo(0, 0);
+
+  // Ändere die Hintergrundfarbe der gradient-box basierend auf der aktiven Sektion
+  const gradientBox = document.querySelector('.gradient-box');
+  gradientBox.classList.remove('home', 'games', 'about', 'lebenslauf'); // Entferne alle spezifischen Klassen
+  if (sectionId === 'home') {
+    gradientBox.classList.add('home');
+  } else if (sectionId === 'games') {
+    gradientBox.classList.add('games');
+  }else if (sectionId === 'about') {
+    gradientBox.classList.add('about');
+  }else if (sectionId === 'lebenslauf') {
+    gradientBox.classList.add('lebenslauf');
+  }
 }
